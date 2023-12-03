01508cc30f6953dc1769ff465b76a7a5c2caf2f9
Santos-256 <santosedward1998@gmail.com>
2023-12-03 06:04:28 -0800

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    for last_digit in number:
+   # for last_digit in number:
         if last_digit < 0:
             last_digit = number % -10
         elif last_digit > 0:
