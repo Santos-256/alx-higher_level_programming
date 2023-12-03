50b2016c9ded191949883cf6d1dc861892626c48
Santos-256 <santosedward1998@gmail.com>
2023-12-03 06:02:07 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    for last_digit in number:
+        if last_digit < 0:
+            last_digit = number % -10
+        elif last_digit > 0:
+            last_digit = number % 10
+        print("{:d}".format(last_digit), end=' ')
+        return last_digit
