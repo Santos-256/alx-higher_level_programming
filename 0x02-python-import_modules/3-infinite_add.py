1df41e2262ce7aa6b8d135973700b7106a0dd5f4
Santos-256 <santosedward1998@gmail.com>
2023-12-07 03:47:46 -0800

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    sum = 0
+    for index in range(len(sys.argv) - 1):
+        sum += int(sys.argv[index + 1])
+    print("{}".format(sum))
