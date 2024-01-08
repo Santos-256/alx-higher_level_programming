da9147be2eec3a50d63741f78dce85b8198edba9
Santos-256 <santosedwar1998@gmail.com>
2024-01-08 02:55:49 -0800

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
-
+"""Defines lookup function"""
 def lookup(obj):
-        """Returns a list of an object's available attributes."""
-            return (dir(obj))
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
