a859a9472868a61c754e49dd266cdef3e1f68569
Santos-256 <santosedwar1998@gmail.com>
2024-01-12 02:22:41 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines lookup function"""
+
+
 def lookup(obj):
     """Return a list of an object's available attributes."""
     return (dir(obj))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines class MyList."""
 
+
 class MyList(list):
     """Implements sorted printing for the built-in list class"""
-
 
     def print_sorted(self):
         """prints a list in sorted ascending order"""
