f481ce558ea7792a476be386244a597efe78b725
Santos-256 <santosedwar1998@gmail.com>
2024-01-12 02:53:51 -0800

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class.
+
+
+    Args:
+        obj (any): The object to be checked.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
+    """
     if isinstance(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
