40f5564425c80189464b034b2cccd5e9bf54043e
Santos-256 <santosedwar1998@gmail.com>
2024-03-13 01:07:29 +0000

    update

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,6 @@
-#!/usr/bin/node
+#!/usr/bin/node //The Shebang
+ // The class definition for Rectangle
 class Rectangle {
 }
-
+// Exporting the Rectangle class for use in other modules
 module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,13 @@
-#!/usr/bin/node
+#!/usr/bin/node //shebang saticifying the interpreter to use (Node.js)
+
 exports.nbOccurences = function (list, searchElement) {
-	  let nOccurrences = 0;
-	  for (let i = 0; i < list.length; i++) {
-		      if (searchElement === list[i]) {
-			            nOccurrences++;
-			          }
-		    }
-	  return nOccurrences;
+	 // Exporting a function named nbOccurences
+	// The function takes two parameters: list (the array to search in) and searchElement (the element to count occurrences of)
+	let nOccurrences = 0;
+	for (let i = 0; i < list.length; i++) {
+		if (searchElement === list[i]) {
+			nOccurrences++;
+		}
+	}
+	return nOccurrences;
 };
