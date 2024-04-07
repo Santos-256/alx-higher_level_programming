c98c5fe4eec87b161475c8f18eed1c8d6f0afd6a
Santos Edward <132166054+Santos-256@users.noreply.github.com>
2024-04-07 01:46:35 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node //The Shebang
- // The class definition for Rectangle
-class Rectangle {
-}
-// Exporting the Rectangle class for use in other modules
-module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-// Importing the 'list' array from the '100-data.js' file
-const list = require('./100-data.js').list;
-// Printing the original 'list' array
-console.log(list);
-// Mapping over the elements of the 'list' array and multiplying each element by its index
-console.log(list.map((item, index) => item * index));
